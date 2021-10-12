be9bf57c5ed107e97cc1411a9b8391d86c5bd674
Gabriela Valencia <73788811+GabrielaSValencia@users.noreply.github.com>
2021-10-12 18:01:20 -0300

    Add files via upload
    Curso Python #04 - Primeiros comandos em Python3 - Curso em vídeo 
    https://www.youtube.com/watch?v=31llNGKWDdo

---
+++ Curso_Python04.py
@@ -0,0 +1,7 @@
+print("Iniciando o aprendizado com Python!")
+nome='Gabriela'
+idade='35 anos'
+print(nome,idade)
+nome=input('Qual é o seu nome?')
+idade=input('Qual a sua idade?')
+print(nome,idade)
